2aefd338602eb1f465d6aeb4b73c23356e70b493
Virjanand Panday <virjanand@virjanand.com>
2022-07-14 09:49:02 +0200

    change to Function to be able to extract

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,6 +1,7 @@
 package fizzbuzz;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,21 +9,21 @@ import java.util.stream.Stream;
 
 public class FizzBuzz {
     public List<String> getNumbers(int from, int until) {
-        return IntStream.rangeClosed(from, until).mapToObj(number -> toFizzBuzz(number, Stream.of(() -> {
-            if (number % 3 == 0) {
+        return IntStream.rangeClosed(from, until).mapToObj(number -> toFizzBuzz(number, Stream.of(number1 -> {
+            if (number1 % 3 == 0) {
                 return "Fizz";
             }
             return "";
-        }, () -> {
-            if (number % 5 == 0) {
+        }, number1 -> {
+            if (number1 % 5 == 0) {
                 return "Buzz";
             }
             return "";
         }))).collect(Collectors.toList());
     }
 
-    private String toFizzBuzz(int number, Stream<Supplier<String>> rules) {
-        String result = rules.map(Supplier::get).collect(Collectors.joining());
+    private String toFizzBuzz(int number, Stream<Function<Integer, String>> rules) {
+        String result = rules.map(rule -> rule.apply(number)).collect(Collectors.joining());
         if (result.isEmpty()) {
             return Integer.toString(number);
         }
