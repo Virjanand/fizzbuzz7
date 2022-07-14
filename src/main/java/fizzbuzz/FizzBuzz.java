2c342a224e5f15ca22143a077d85845d6a8f4784
Virjanand Panday <virjanand@virjanand.com>
2022-07-14 09:54:25 +0200

    Optimize imports

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,6 @@ package fizzbuzz;
 
 import java.util.List;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
